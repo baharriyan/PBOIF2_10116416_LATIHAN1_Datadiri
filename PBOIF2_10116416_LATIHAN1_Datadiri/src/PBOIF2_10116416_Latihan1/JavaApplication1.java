9410d2b2f1cc790c7c18a9d42c3f3f673ef804b5
baharriyan <indonesiariyan@gmail.com>
2020-10-20 18:30:38 +0700

    p

---
+++ PBOIF2_10116416_LATIHAN1_Datadiri/src/PBOIF2_10116416_Latihan1/JavaApplication1.java
@@ -0,0 +1,31 @@
+/*
+ 
+ */
+package PBOIF2_10116416_Latihan1;
+
+/**
+ *
+ * @author Riyan Bahar
+ * Nama  : Riyan Bahar
+ * Kelas : IF-2
+ * Nim   : 10116416
+ * 
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println("BIODATA DIRI" + "\n" + "-------------------");
+      System.out.println("NAMA\t\t: Riyan Bahar");
+      System.out.println("ALAMAT\t\t: Jalan Tubagus Ismail Dalam No.31");
+      System.out.println("TEMPAT LAHIR\t: subang, jawab barat");
+      System.out.println("TANGGAL LAHIR\t: 4 maret 1998");
+      System.out.println("--------------------------");
+      System.out.println("HOBI\t\t: sepakbola ");
+      System.out.println("MAKES\t\t: ayam krebek");
+      System.out.println("MIKES\t\t: Kopi Susu");
+    }
+    
+}
